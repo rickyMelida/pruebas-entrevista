f50e09d905937991fbfbadfcf3633d0070df1a6e
Ricardo Mélida <ricardomelida92@gmail.com>
2021-02-12 14:13:23 -0300

    Se agregan cards de videos
    Se han agrega cards de videos en la seccion de transmision de videojuegos.
    Tambien se ha modificado padding de caja en linea de tal forma que esta tenga un pequeño margen en la parte superior

--- prueba1/js/main.js
+++ prueba1/js/main.js
@@ -1,18 +1,13 @@
 let card_online = document.querySelector('.cards-online');
 let other_col = document.querySelector('.other-col');
+let online_games = document.querySelector('.online-games');
 
 let card = document.querySelector('.card-video');
 
 
-for (let i = 1; i < 4; i++) {
-    let card_2 = card.cloneNode(true);
-    card_online.appendChild(card_2);
-}
-
-for (let i = 0; i < 4; i++) {
-    let card_3 = card.cloneNode(true);
-    other_col.appendChild(card_3);
-}
+duplicateCard(3, card, card_online)
+duplicateCard(4, card, other_col);
+duplicateCard(4, card, online_games);
 
 let card_image = document.querySelectorAll('.row .card-video .card-img-top');
 
@@ -21,4 +16,10 @@ for (let i = 0; i < card_image.length; i++) {
     card_image[i].setAttribute('src', `./images/cards/${i+1}.jpg`)    
 }
 
-console.log(card_image.length);
+
+function duplicateCard(cant_card, cardOrigin, container) {
+    for (let i = 1; i <= cant_card; i++) {
+        let card_res = cardOrigin.cloneNode(true);
+        container.appendChild(card_res);
+    }
+}
