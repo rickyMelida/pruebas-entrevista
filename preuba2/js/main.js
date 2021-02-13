b6cd4f55b96f83cd0287426ef549289d475e1b59
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-02-13 06:02:42 -0300

    Se agregan elementos y estilos en prueba 2
    Se han agregado los elementos faltantes en la prueba 2, asi tambien se
    estilizaron con bootstrap.
    Aun faltan verificar algunos pequeños detalles que tienen mucha
    relevancia con relacion al diseño original.

--- preuba2/js/main.js
+++ preuba2/js/main.js
@@ -1,25 +1,31 @@
-let card_online = document.querySelector('.cards-online');
-let other_col = document.querySelector('.other-col');
-let online_games = document.querySelector('.online-games');
+let menu_user = document.querySelector('.menu-user');
+let item_menu_user = document.querySelector('#item-menu-user');
 
-let card = document.querySelector('.card-video');
+let section_notifications = document.querySelector('.section-notifications');
+let notifications = document.querySelector('.notification-all');
+
+let icons = ['fa-users', 'fa-comments', 'fa-heart', 'fa-coins', 'fa-store', 'fa-calendar', 'fa-envelope-open'];
+let names_items = ['Privilegiados', 'Comentarios', 'Me gusta', 'Propinas', 'Tienda', 'Calendarios', 'Buzon de sugerencias'];
+
+let first_icon_point = document.querySelector('.f-icon');
 
 
-duplicateCard(3, card, card_online)
-duplicateCard(4, card, other_col);
-duplicateCard(4, card, online_games);
 
-let card_image = document.querySelectorAll('.row .card-video .card-img-top');
 
-for (let i = 0; i < card_image.length; i++) {
-    card_image[i].removeAttribute('src');
-    card_image[i].setAttribute('src', `./images/cards/${i+1}.jpg`)    
+for (let i = 0; i < icons.length; i++) {
+    let new_item = item_menu_user.cloneNode(true);
+    new_item.childNodes[1].classList.remove('fa-th-large');
+    new_item.childNodes[1].classList.add(icons[i]);
+    new_item.childNodes[2].nodeValue = names_items[i];
+    menu_user.appendChild(new_item);
 }
 
+for (let j = 0; j < 7; j++) {
+    let new_element = notifications.cloneNode(true);
+    section_notifications.appendChild(new_element);
+}
 
-function duplicateCard(cant_card, cardOrigin, container) {
-    for (let i = 1; i <= cant_card; i++) {
-        let card_res = cardOrigin.cloneNode(true);
-        container.appendChild(card_res);
-    }
+// first_icon_point.removeChild(first_icon_point.childNodes[0]);
+function duplicate_and_modify_icons(element_origin, new_icon) {
+
 }
